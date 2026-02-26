f6aeb9d3b5db7b136d91eff3e85c5e861345ef3a
Student <Student@DESKTOP-IL21489>
2026-02-26 15:41:06 +0300

    vrode_rabotaet?

---
+++ pulicGit_guessNum_WPF/Classes/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace pulicGit_guessNum_WPF.Classes
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> _execute, Func<object, bool> _canExecute = null)
+        {
+            this._execute = _execute;
+            this._canExecute = _canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ pulicGit_guessNum_WPF/ViewModels/GameViewModels.cs
@@ -0,0 +1,120 @@
+﻿using pulicGit_guessNum_WPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace pulicGit_guessNum_WPF.ViewModels;
+
+public class GameViewModels : INotifyPropertyChanged
+{
+    public ICommand NewGameCommand { get; }
+    public ICommand GuessCommand { get; }
+    public GameViewModels()
+    {
+        NewGame(null);
+        NewGameCommand = new RelayCommand(NewGame);
+        GuessCommand = new RelayCommand(GuessNumber, CanGuessNumber);
+    }
+
+    public int PeopleNumber
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged(nameof(GuessNumber));
+        }
+    }
+    public int RandNumber
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged(nameof(RandNumber));
+        }
+    }
+    public string ErrorMessage
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged(nameof(ErrorMessage));
+        }
+    }
+    public int Attempt
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged(nameof(Attempt));
+        }
+    }
+    public int Score
+    {
+        get => field;
+        set
+        {
+            field = value;
+            OnPropertyChanged(nameof(Score));
+        }
+    }
+
+    private void NewGame(object parameter)
+    {
+        Score = 0;
+        Attempt = 25;
+        ErrorMessage = "";
+    }
+
+    private void GuessNumber(object parameter)
+    {
+        GenerateNumber();
+        if (PeopleNumber > RandNumber)
+        {
+            ErrorMessage = "Число меньше! Попробуй ещё раз";
+            Score++;
+            Attempt--;
+        }
+        else if (PeopleNumber < RandNumber)
+        {
+            ErrorMessage = "Число больше! Попробуй ещё раз";
+            Score++;
+            Attempt--;
+        }
+        else if(PeopleNumber == RandNumber)
+        {
+            ErrorMessage = "Ты угадал число!";
+            Score++;
+            NewGame(null);
+        }
+
+        if(Attempt == 0)
+        {
+            Attempt = 0;
+            NewGame(null);
+        }
+    }
+    private bool CanGuessNumber(object parameter)
+    {
+        return !string.IsNullOrEmpty(parameter.ToString());
+    }
+
+    private int GenerateNumber()
+    {
+        Random random = new Random();
+        RandNumber = random.Next(1, 1000);
+        return RandNumber;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
