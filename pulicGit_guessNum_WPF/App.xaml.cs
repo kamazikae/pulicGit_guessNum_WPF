750b441e254088b9fe2b371447a8692ebe5d0735
Student <Student@DESKTOP-IL21489>
2026-02-26 14:41:48 +0300

    Добавьте файлы проекта.

---
+++ pulicGit_guessNum_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pulicGit_guessNum_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
